fbc1ee761d4ccb96fc2ad8b164c0a015b14bd24e
Jan-Ole Stein <janolestein@gmail.com>
2025-04-09 20:34:49 +0200

    small updates to lazyvim

---
+++ nvim/.config/nvim/lua/plugins/themes.lua
@@ -0,0 +1,45 @@
+return {
+  {
+    "folke/tokyonight.nvim",
+    opts = {
+      transparent = true,
+      styles = {
+        sidebars = "transparent",
+        floats = "transparent",
+      },
+    },
+  },
+  -- { "EdenEast/nightfox.nvim" },
+  -- {
+  --   "LazyVim/LazyVim",
+  --   opts = {
+  --     colorscheme = "nightfox",
+  --   },
+  -- },
+  -- { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+  -- require("catppuccin").setup({
+  --   flavour = "latte", -- latte, frappe, macchiato, mocha
+  --   background = {     -- :h background
+  --     light = "latte",
+  --     dark = "latte",
+  --   },
+  -- }),
+  --
+  -- {
+  --   "LazyVim/LazyVim",
+  --   opts = {
+  --     colorscheme = "catppuccin",
+  --   },
+  -- },
+  -- gruvbox theme
+  --   { "ellisonleao/gruvbox.nvim" },
+  --
+  --   -- Configure LazyVim to load gruvbox
+  --   {
+  --     "LazyVim/LazyVim",
+  --     opts = {
+  --       colorscheme = "gruvbox",
+  --     },
+  --   },
+  -- },
+}
