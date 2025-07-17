5d067aaa8c15780c1db42582ec932042ab253c1d
Jan-Ole Stein <janolestein@gmail.com>
2025-07-17 19:31:45 +0200

    remove: autocmd format

--- nvim/.config/nvim/lua/config/autocmds.lua
+++ nvim/.config/nvim/lua/config/autocmds.lua
@@ -8,17 +8,17 @@
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
 
 -- auto format on save
-vim.api.nvim_create_autocmd("LspAttach", {
-  group = vim.api.nvim_create_augroup("lsp", { clear = true }),
-  callback = function(args)
-    -- 2
-    vim.api.nvim_create_autocmd("BufWritePre", {
-      -- 3
-      buffer = args.buf,
-      callback = function()
-        -- 4 + 5
-        vim.lsp.buf.format({ async = false, id = args.data.client_id })
-      end,
-    })
-  end,
-})
+-- vim.api.nvim_create_autocmd("LspAttach", {
+--   group = vim.api.nvim_create_augroup("lsp", { clear = true }),
+--   callback = function(args)
+--     -- 2
+--     vim.api.nvim_create_autocmd("BufWritePre", {
+--       -- 3
+--       buffer = args.buf,
+--       callback = function()
+--         -- 4 + 5
+--         vim.lsp.buf.format({ async = false, id = args.data.client_id })
+--       end,
+--     })
+--   end,
+-- })
